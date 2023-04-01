1757f3e638cd1ff056fb522086e6ded368e3c2bf
h3nrey <pedro.novaes.education@gmail.com>
2023-03-31 22:15:31 -0300

    basic player mechanics

--- init.py
+++ init.py
@@ -13,45 +13,62 @@ clock = pygame.time.Clock();
 FPS = 60;
 
 # FONTS
-textFont = pygame.font.Font("font/Pixeltype.ttf", 30);
+textFont = pygame.font.Font("font/Pixeltype.ttf", 40);
 
 
 # SURFACES
 skySurface = pygame.image.load("Assets/Graphics/Sky.png").convert();
 groundSurface = pygame.image.load("Assets/Graphics/ground.png").convert()
-scoreText = textFont.render('Score: 100', False, "red");
+scoreText = textFont.render('Score: 100', False, (80,80,80));
+scoreRect = scoreText.get_rect(center = (screen.get_width() / 2, 40))
 
 playerSurf = pygame.image.load("Assets/Graphics/Player/player_walk_1.png").convert_alpha();
 playerRect = playerSurf.get_rect(midbottom = (50, 300));
+gravity = 0;
+jumpForce = -20;
+canJump = True;
  
 snailSurf = pygame.image.load("Assets/Graphics/snail/snail1.png").convert_alpha()
 snailRect = snailSurf.get_rect(midbottom = (750, 300))
 snailPosX = 700
 
+line = pygame.Surface((10,10))
 # Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit();
             exit(); # this is just a more acurate way of put a break here
+        
+        if(canJump): 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                gravity = jumpForce;
+            if event.type == pygame.KEYDOWN:
+                if(event.key == pygame.K_SPACE):
+                    gravity = jumpForce;
+        canJump = False;
     
     screen.blit(skySurface, (0,0));
     screen.blit(groundSurface, (0,300));
-    screen.blit(scoreText, (20, 10));
+    pygame.draw.rect(screen, "#aaa1ed", scoreRect)
+    pygame.draw.rect(screen, "#aaa1ed", scoreRect, 10)
+    screen.blit(scoreText, scoreRect);
     
-    screen.blit(playerSurf, playerRect);
 
     snailSpeed = 7;
     snailRect.left -= snailSpeed;
     if (snailRect.left <= -100): snailRect.left = 750;
     screen.blit(snailSurf, snailRect);
 
-    # if(playerRect.colliderect(snailRect)):
-    #     print("player touched snail")
+    # Player
+    gravity += 1;
+    playerRect.y += gravity;
 
-    mousePos = pygame.mouse.get_pos();
-    if(playerRect.collidepoint(mousePos)):
-        print("mouse hovering player")
+    if(playerRect.bottom >= 300): 
+        canJump = True;
+        playerRect.bottom = 300;
+    
+    screen.blit(playerSurf, playerRect);
 
     # Draw all elements
     pygame.display.update();
